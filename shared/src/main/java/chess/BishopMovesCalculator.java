7d2713d273970a63e54b307ac46173e7c0c317ce
Tad Rosenberg <tad@swelle.io>
2024-09-13 16:36:24 -0600

    got initial moves working for bishop

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -23,7 +23,7 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
         // Implement logic to calculate king's moves
-        for (ChessPosition pos : getBishopMoves(position)) {
+        for (ChessPosition pos : getBishopMoves(board, position)) {
             if (board.getPiece(pos) == null) {
                 moves.add(new ChessMove(position, pos, null));
             } else if (board.getPiece(pos).getTeamColor() != board.getPiece(position).getTeamColor()) {
@@ -33,8 +33,53 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
         return moves;
     }
 
-    private Collection<ChessPosition> getBishopMoves(ChessPosition myPosition) {
+    private Collection<ChessPosition> getBishopMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessPosition> bishopMoves = new ArrayList<>();
+        int currentRow = myPosition.getRow();
+        int currentCol = myPosition.getColumn();
+        int[][] directions = {
+                {1, 1},   // Up-Right
+                {1, -1},  // Up-Left
+                {-1, 1},  // Down-Right
+                {-1, -1}  // Down-Left
+        };
+
+        // Iterate over each direction
+        for (int[] direction : directions) {
+            int rowOffset = direction[0];
+            int colOffset = direction[1];
+            int newRow = currentRow + rowOffset;
+            int newCol = currentCol + colOffset;
+
+            // Continue moving in the current direction until the move is invalid
+            while (isWithinBounds(newRow, newCol)) {
+                ChessPosition newPosition = new ChessPosition(newRow, newCol);
+
+                // Check if the new position is blocked
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+                if (pieceAtNewPosition == null) {
+                    // If no piece is at the new position, it's a valid move
+                    bishopMoves.add(newPosition);
+                } else {
+                    // If there is a piece, check if it can be captured
+                    if (pieceAtNewPosition.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                        // Can capture the piece
+                        bishopMoves.add(newPosition);
+                    }
+                    // Stop in this direction after encountering any piece
+                    break;
+                }
+
+                // Move further in the same direction
+                newRow += rowOffset;
+                newCol += colOffset;
+            }
+        }
+
         return bishopMoves;
+    }
+
+    private boolean isWithinBounds(int row, int col) {
+        return row >= 0 && row < 8 && col >= 0 && col < 8; // Assuming an 8x8 chessboard
     }
 }
