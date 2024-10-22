86125c762cecff5d8e6649eb879da4072a5c9c70
Tad Rosenberg <tad@swelle.io>
2024-10-22 16:58:10 -0600

    shortened name

--- shared/src/main/java/chess/MoveUtils.java
+++ shared/src/main/java/chess/MoveUtils.java
@@ -3,16 +3,16 @@ package chess;
 import java.util.Collection;
 
 public class MoveUtils {
-    public static void addValidMoves(Collection<ChessMove> moves, ChessBoard board, ChessPosition startPosition, Collection<ChessPosition> possiblePositions) {
-        ChessPiece startPiece = board.getPiece(startPosition);
+    public static void addValidMoves(Collection<ChessMove> moves, ChessBoard board, ChessPosition sPosition, Collection<ChessPosition> positions) {
+        ChessPiece startPiece = board.getPiece(sPosition);
 
-        for (ChessPosition endPosition : possiblePositions) {
+        for (ChessPosition endPosition : positions) {
             ChessPiece endPiece = board.getPiece(endPosition);
 
             if (endPiece == null) {
-                moves.add(new ChessMove(startPosition, endPosition, null));
+                moves.add(new ChessMove(sPosition, endPosition, null));
             } else if (endPiece.getTeamColor() != startPiece.getTeamColor()) {
-                moves.add(new ChessMove(startPosition, endPosition, null));
+                moves.add(new ChessMove(sPosition, endPosition, null));
             }
         }
     }
