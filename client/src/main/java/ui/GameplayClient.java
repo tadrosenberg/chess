d4c4a2fd08d2e98de31a4954d3792132fc07eb1e
Tad Rosenberg <tad@swelle.io>
2024-12-03 22:50:20 -0700

    added make move command

---
+++ client/src/main/java/ui/GameplayClient.java
@@ -0,0 +1,39 @@
+package ui;
+
+import chess.ChessMove;
+import exception.ServiceException;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+
+public class GameplayClient {
+    private final WebSocketFacade webSocketFacade;
+    private final int gameID;
+    private final String authToken;
+
+    public GameplayClient(WebSocketFacade webSocketFacade, int gameID, String authToken) {
+        this.webSocketFacade = webSocketFacade;
+        this.gameID = gameID;
+        this.authToken = authToken;
+    }
+
+    public void connect() throws ServiceException {
+        var connectCommand = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+        webSocketFacade.sendCommand(connectCommand);
+    }
+
+    public void makeMove(ChessMove move) throws ServiceException {
+        MakeMoveCommand moveCommand = new MakeMoveCommand(authToken, gameID, move);
+        webSocketFacade.sendCommand(moveCommand);
+    }
+
+    public void leaveGame() throws ServiceException {
+        var leaveCommand = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+        webSocketFacade.sendCommand(leaveCommand);
+        webSocketFacade.close();
+    }
+
+    public void resignGame() throws ServiceException {
+        var resignCommand = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID);
+        webSocketFacade.sendCommand(resignCommand);
+    }
+}
